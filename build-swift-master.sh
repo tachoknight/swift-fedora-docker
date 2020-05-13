dba376c5a58b1fac1a0b6ba46e298f25e26004c2
Ron Olson <tachoknight@gmail.com>
2020-05-13 16:11:53 -0500

    Added writing to output file for debugging builds

--- build-swift-master.sh
+++ build-swift-master.sh
@@ -36,7 +36,7 @@ docker run \
 -v $BUILD_DIR:/home/build-user:z \
 -w /home/build-user/ \
 $FEDORAIMAGE \
-/bin/bash -lc "cd /source; git clone https://github.com/apple/swift.git swift; ./swift/utils/update-checkout --clone --scheme master; ./swift/utils/build-script --preset buildbot_linux install_destdir=/home/build-user/builds installable_package=/home/build-user/swift-master.tar.gz"
+/bin/bash -lc "cd /source; git clone https://github.com/apple/swift.git swift; ./swift/utils/update-checkout --clone; ./swift/utils/build-script --preset buildbot_linux install_destdir=/home/build-user/builds installable_package=/home/build-user/swift-master.tar.gz > /home/build-user/build-output.txt"
 
 echo ___ D O N E ___
 echo Source is in $SOURCE_DIR
